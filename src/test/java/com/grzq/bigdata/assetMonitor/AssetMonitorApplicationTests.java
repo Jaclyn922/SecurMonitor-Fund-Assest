261b7ac185278aa939d1a0f2a45c0b5384a0e842
陈杰 <chenjie-xx@grzq.com>
2024-06-04 11:14:36 +0800

    资产监控

---
+++ src/test/java/com/grzq/bigdata/assetMonitor/AssetMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grzq.bigdata.assetMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
